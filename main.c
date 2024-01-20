8c6799274da6d991deceb905fb6c0c3febacde33
sacr1ficerq <sacr1ficerq@gmail.com>
2024-01-20 15:21:24 +0300

    added .gitignore

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
+#include "printer.h"
 
 int main() {
-        
+    printf("hello world!\n");
 }
---
+++ printer.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print(int num) {
+    printf("num:\t%d\n", num);
+}
